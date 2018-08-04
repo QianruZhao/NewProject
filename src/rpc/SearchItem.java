e30ea71cc6f2e940ffa55e406eaef223ceefd3fa
Qianru Zhao <qz@QianrudeMacBook-Pro.local>
2018-08-04 06:32:34 -0400

    first commit

---
+++ src/rpc/SearchItem.java
@@ -0,0 +1,127 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
+import entity.Item;
+import external.TicketMasterAPI;
+
+/**
+ * Servlet implementation class SearchItem
+ */
+@WebServlet(name = "search", urlPatterns = { "/search" })
+//@WebServlet("/search")
+public class SearchItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+//		PrintWriter out = response.getWriter();
+//		
+//		if (request.getParameter("username") != null) {
+//			String username = request.getParameter("username");
+//			out.print("Hello " + username);
+//		}
+//		
+//		out.close();
+		
+//		response.setContentType("application/json");
+//		PrintWriter out = response.getWriter();
+		
+		//---------------------------------------------------------------------
+//		String username = "";
+//		if (request.getParameter("username") != null) {
+//			username = request.getParameter("username");
+//		}
+//		JSONObject obj = new JSONObject();
+//		try {
+//			obj.put("username", username);
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+//		RpcHelper.writeJsonObject(response, obj);
+		//http://localhost:8080/Jupiter/search?username=abcd
+		//-------------------------------
+		
+//		double lat = Double.parseDouble(request.getParameter("lat"));
+//		double lon = Double.parseDouble(request.getParameter("lon"));
+//		
+//		// term can be empty
+//		String term = request.getParameter("term");
+//		
+//
+//		
+//		DBConnection connection = DBConnectionFactory.getConnection();
+//		try {
+//			List<Item> items = connection.searchItems(lat, lon, term);
+//
+//			JSONArray array = new JSONArray();
+//			for (Item item : items) {
+//				array.put(item.toJSONObject());
+//			}
+//			RpcHelper.writeJsonArray(response, array);
+//		} finally {
+//			connection.close();
+//		}
+
+		double lat = Double.parseDouble(request.getParameter("lat"));
+		double lon = Double.parseDouble(request.getParameter("lon"));
+		String term = request.getParameter("term");
+		String userId = request.getParameter("user_id");
+		
+		DBConnection connection = DBConnectionFactory.getConnection();
+		try {
+			List<Item> items = connection.searchItems(lat, lon, term);
+			Set<String> favoriteItems = connection.getFavoriteItemIds(userId);
+
+			JSONArray array = new JSONArray();
+			for (Item item : items) {
+				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favoriteItems.contains(item.getItemId()));
+				array.put(obj);
+			}
+			RpcHelper.writeJsonArray(response, array);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			connection.close();
+		}
+
+
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
